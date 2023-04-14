aff03e5085b8c0c0b1cd1fc99a14509ff6aa2c1e
cleitomedeiros <cleitovertical@gmail.com>
2023-04-14 12:51:37 +0000

    El boton ocupa toda la pantalla

--- src/front/js/component/button.jsx
+++ src/front/js/component/button.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Button(props) {
-  return <button onClick={props.onClick}>{props.label}</button>;
+  return (
+    <div className="d-grid gap-2 col-12 mx-auto">
+      <button className="btn btn-primary" onClick={props.onClick}>
+        {props.label}
+      </button>
+    </div>
+  );
 }
 
 export default Button;
