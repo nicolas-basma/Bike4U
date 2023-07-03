bf1ff3333874e17d52e7576bf61556e2006df7ec
Luis Carlos Loredo Bellido <lloredo.eng@gmail.com>
2023-07-03 23:53:14 +0200

    User Data width reduced for lg screen

--- src/front/js/views/EditUserData/EditUserData.jsx
+++ src/front/js/views/EditUserData/EditUserData.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { FormattedMessage } from "react-intl";
-import { useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from "react-intl";
 
 import useStore from "../../store/AppContext.jsx";
 import useForms from "../../utils/useForms.jsx";
@@ -157,174 +156,176 @@ const buttonState = myButtonColorBoolean() ? "sendBtn" : "deleteBtn";
     
   return (
     <form>
-      <div className="wrapper">
-        <div className="signUpFirstTitle">
-          <h1> <FormattedMessage id="modifyTitle" defaultMessage="DATOS PERSONALES"/> </h1>
-        </div>
-
-        <div className="row mb-3">
-          <div className="col-12">
-            <label htmlFor="formName" className="form-label">
-              <FormattedMessage id="signInName" defaultMessage="Nombre"/>
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="formName"
-              aria-describedby="emailHelp"
-              placeholder="Introduzca su nombre"
-              value={formInput["name"]}
-              onChange={myHandleInput}
-              name="name"
-            />
+      <div className="wrapper d-flex justify-content-center">
+        <div className="col-12 col-md-10 col-lg-8">
+          <div className="signUpFirstTitle">
+            <h1> <FormattedMessage id="modifyTitle" defaultMessage="DATOS PERSONALES"/> </h1>
           </div>
-        </div>
 
-        <div className="row mb-3">
-          <div className="col-12">
-            <label htmlFor="formLastName" className="form-label">
-            <FormattedMessage id="signInLastName" defaultMessage="Apellidos"/>
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="formLastName"
-              aria-describedby="emailHelp"
-              placeholder="Introduzca sus apellidos"
-              name="lastname"
-              value={formInput["lastname"]}
-              onChange={myHandleInput}             
-            />
+          <div className="row mb-3">
+            <div className="col-12">
+              <label htmlFor="formName" className="form-label">
+                <FormattedMessage id="signInName" defaultMessage="Nombre"/>
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="formName"
+                aria-describedby="emailHelp"
+                placeholder="Introduzca su nombre"
+                value={formInput["name"]}
+                onChange={myHandleInput}
+                name="name"
+              />
+            </div>
           </div>
-        </div>
 
-        <div className="row mb-3">
-          <div className="col-12">
-            <label htmlFor="formEmail" className="form-label">
-            <FormattedMessage id="signInEmail" defaultMessage="Email"/>
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="formEmail"
-              placeholder="Ej. juan@perez.com"
-              name="email"
-              value={formInput["email"]}
-              onChange={myHandleInput}
-            />
+          <div className="row mb-3">
+            <div className="col-12">
+              <label htmlFor="formLastName" className="form-label">
+              <FormattedMessage id="signInLastName" defaultMessage="Apellidos"/>
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="formLastName"
+                aria-describedby="emailHelp"
+                placeholder="Introduzca sus apellidos"
+                name="lastname"
+                value={formInput["lastname"]}
+                onChange={myHandleInput}             
+              />
+            </div>
           </div>
-        </div>
-    
-        <div className="row mb-3">
-          <div className="col-12">
-            <label htmlFor="formSizeSelect" className="form-label">
-            <FormattedMessage id="modifyHeight" defaultMessage="Altura"/>
-            </label>
-            <select id="formSizeSelect" onChange={myHandleInput} name="height" className="form-select" value={formInput["height"]}>
-              <option >{intl.formatMessage({ id: 'signInOptionHeight' })}</option>
-              <option value={"xs"}>150-160 cm</option>
-              <option value={"s"}>161-170 cm</option>
-              <option value={"m"}>171-180 cm</option>
-              <option value={"l"}>181-190 cm</option>
-              <option value={"xl"}>+ 190 cm</option>
-            </select>
+
+          <div className="row mb-3">
+            <div className="col-12">
+              <label htmlFor="formEmail" className="form-label">
+              <FormattedMessage id="signInEmail" defaultMessage="Email"/>
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="formEmail"
+                placeholder="Ej. juan@perez.com"
+                name="email"
+                value={formInput["email"]}
+                onChange={myHandleInput}
+              />
+            </div>
           </div>
-        </div>
-
-        <div className="row mb-3">
-          <div className="col-12">
-            <label htmlFor="formWeightSelect" className="form-label">
-              <FormattedMessage id="modifyWeight" defaultMessage="Peso"/>
-            </label>
-            <select id="formWeightSelect" onChange={myHandleInput} className="form-select" name="weight" value={formInput["weight"]}>
-              <option>{intl.formatMessage({ id: 'signInOptionWeight' })}</option>
-              <option>30-40 kg</option>
-              <option>41-50 kg</option>
-              <option>51-60 kg</option>
-              <option>61-70 kg</option>
-              <option>71-80 kg</option>
-              <option>81-90 kg</option>
-              <option>91-100 kg</option>
-              <option>+100 kg</option>
-            </select>
+      
+          <div className="row mb-3">
+            <div className="col-12">
+              <label htmlFor="formSizeSelect" className="form-label">
+              <FormattedMessage id="modifyHeight" defaultMessage="Altura"/>
+              </label>
+              <select id="formSizeSelect" onChange={myHandleInput} name="height" className="form-select" value={formInput["height"]}>
+                <option >{intl.formatMessage({ id: 'signInOptionHeight' })}</option>
+                <option value={"xs"}>150-160 cm</option>
+                <option value={"s"}>161-170 cm</option>
+                <option value={"m"}>171-180 cm</option>
+                <option value={"l"}>181-190 cm</option>
+                <option value={"xl"}>+ 190 cm</option>
+              </select>
+            </div>
           </div>
-        </div>
 
-        <div className="row mb-3">
-          <div className="col-12">
-            <label htmlFor="formBikeSelect" className="form-label" >
-              <FormattedMessage id="signInBikeType" defaultMessage="Tipo de bicicleta"/>
-            </label>
-            <select id="formBikeSelect" onChange={myHandleInput} className="form-select" name="bike_type" value={formInput["bike_type"]}>
-              <option>{intl.formatMessage({ id: 'signInOptionBikeType' })}</option>
-              <option value={"road"}>{intl.formatMessage({ id: 'signInOptionBikeRoad' })}</option>
-              <option value={"mtb"}>{intl.formatMessage({ id: 'signInOptionBikeMTB' })}</option>
-              <option value={"urban"}>{intl.formatMessage({ id: 'signInOptionBikeUrban' })}</option>
-            </select>
+          <div className="row mb-3">
+            <div className="col-12">
+              <label htmlFor="formWeightSelect" className="form-label">
+                <FormattedMessage id="modifyWeight" defaultMessage="Peso"/>
+              </label>
+              <select id="formWeightSelect" onChange={myHandleInput} className="form-select" name="weight" value={formInput["weight"]}>
+                <option>{intl.formatMessage({ id: 'signInOptionWeight' })}</option>
+                <option>30-40 kg</option>
+                <option>41-50 kg</option>
+                <option>51-60 kg</option>
+                <option>61-70 kg</option>
+                <option>71-80 kg</option>
+                <option>81-90 kg</option>
+                <option>91-100 kg</option>
+                <option>+100 kg</option>
+              </select>
+            </div>
           </div>
-        </div>
 
-        <button type="button" className="databtn sendBtn" onClick={handleUpdateUser}>
-          <FormattedMessage id="modifyData" defaultMessage="Actualizar"/>
-        </button>
-        <hr />
-
-        <div className="row mb-3">
-          <div className="col-12 col-sm-6 position-relative">
-            <label htmlFor="newPassword" className="form-label">
-              <FormattedMessage id="modifyPass" defaultMessage="Nueva contraseña"/>             
-            </label>
-            <input
-              type={showPassword ? "text" : "password"}
-              className="form-control"
-              id="newPassword"
-              aria-describedby="newPassword"
-              placeholder={intl.formatMessage({ id: 'modifyNewPassPlaceholder' })}
-              name="newPassword"
-              value={formInput["newPassword"]}
-              onChange={myHandleInput}
-            />
-            <button
-                className="btn position-absolute top-50 end-0 me-2"
-                type="button"
-                onClick={handleToggleShowPassword}
-                >
-                  <i className={showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'}></i>
-            </button>
+          <div className="row mb-3">
+            <div className="col-12">
+              <label htmlFor="formBikeSelect" className="form-label" >
+                <FormattedMessage id="signInBikeType" defaultMessage="Tipo de bicicleta"/>
+              </label>
+              <select id="formBikeSelect" onChange={myHandleInput} className="form-select" name="bike_type" value={formInput["bike_type"]}>
+                <option>{intl.formatMessage({ id: 'signInOptionBikeType' })}</option>
+                <option value={"road"}>{intl.formatMessage({ id: 'signInOptionBikeRoad' })}</option>
+                <option value={"mtb"}>{intl.formatMessage({ id: 'signInOptionBikeMTB' })}</option>
+                <option value={"urban"}>{intl.formatMessage({ id: 'signInOptionBikeUrban' })}</option>
+              </select>
+            </div>
           </div>
-        
 
-          <div className="col-12 col-sm-6 position-relative">
-            <label htmlFor="newPasswordValidation" className="form-label">
-              <FormattedMessage id="modifyVerifyPass" defaultMessage="Verifique la contraseña"/>
-            </label>
-            <input
-              type={showPassword ? "text" : "password"}
-              className="form-control"
-              id="newPasswordValidation"
-              aria-describedby="newPasswordValidation"
-              placeholder={intl.formatMessage({ id: 'modifyPassPlaceholder' })}
-              name="newPasswordValidation"
-              value={formInput["newPasswordValidation"]}
-              onChange={myHandleInput}
-            />
-            <button
-                className="btn position-absolute top-50 end-0 me-2"
-                type="button"
-                onClick={handleToggleShowPassword}
-                >
-                  <i className={showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'}></i>
-            </button>
+          <button type="button" className="databtn sendBtn" onClick={handleUpdateUser}>
+            <FormattedMessage id="modifyData" defaultMessage="Actualizar"/>
+          </button>
+          <hr />
+
+          <div className="row mb-3">
+            <div className="col-12 col-sm-6 position-relative">
+              <label htmlFor="newPassword" className="form-label">
+                <FormattedMessage id="modifyPass" defaultMessage="Nueva contraseña"/>             
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                className="form-control"
+                id="newPassword"
+                aria-describedby="newPassword"
+                placeholder={intl.formatMessage({ id: 'modifyNewPassPlaceholder' })}
+                name="newPassword"
+                value={formInput["newPassword"]}
+                onChange={myHandleInput}
+              />
+              <button
+                  className="btn position-absolute top-50 end-0 me-2"
+                  type="button"
+                  onClick={handleToggleShowPassword}
+                  >
+                    <i className={showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'}></i>
+              </button>
+            </div>
+          
+
+            <div className="col-12 col-sm-6 position-relative">
+              <label htmlFor="newPasswordValidation" className="form-label">
+                <FormattedMessage id="modifyVerifyPass" defaultMessage="Verifique la contraseña"/>
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                className="form-control"
+                id="newPasswordValidation"
+                aria-describedby="newPasswordValidation"
+                placeholder={intl.formatMessage({ id: 'modifyPassPlaceholder' })}
+                name="newPasswordValidation"
+                value={formInput["newPasswordValidation"]}
+                onChange={myHandleInput}
+              />
+              <button
+                  className="btn position-absolute top-50 end-0 me-2"
+                  type="button"
+                  onClick={handleToggleShowPassword}
+                  >
+                    <i className={showPassword ? 'fas fa-eye-slash' : 'fas fa-eye'}></i>
+              </button>
+            </div>
           </div>
-        </div>
 
-        <button type="button" className={"databtn "+ buttonState} onClick={handleChangePassword}>
-          <FormattedMessage id="modifyPassBtn" defaultMessage="Modificar contraseña"/>
-        </button>
-        <hr />
-        <button type="button" className="databtn deleteBtn" onClick={handleDeleteUser}>
-          <FormattedMessage id="modifyDeleteBtn" defaultMessage="Eliminar cuenta"/>
-        </button>
+          <button type="button" className={"databtn "+ buttonState} onClick={handleChangePassword}>
+            <FormattedMessage id="modifyPassBtn" defaultMessage="Modificar contraseña"/>
+          </button>
+          <hr />
+          <button type="button" className="databtn deleteBtn" onClick={handleDeleteUser}>
+            <FormattedMessage id="modifyDeleteBtn" defaultMessage="Eliminar cuenta"/>
+          </button>
+        </div>
       </div>
     </form>
   );
